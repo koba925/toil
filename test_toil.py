e859e4b8f56de10efc6289041ccefccfd3a66096
Kobayashi Takahiro <koba925@gmail.com>
2026-02-11 07:12:32 +0000

    if式を評価する

--- test_toil.py
+++ test_toil.py
@@ -5,3 +5,12 @@ def test_evaluate_value():
     assert evaluate(5) == 5
     assert evaluate(True) is True
     assert evaluate(False) is False
+
+def test_evaluate_if():
+    assert evaluate(("if", True, 2, 3)) == 2
+    assert evaluate(("if", False, 2, 3)) == 3
+    assert evaluate(("if", ("if", True, True, False), 2, 3)) == 2
+    assert evaluate(("if", True, ("if", True, 2, 3), 4)) == 2
+    assert evaluate(("if", False, 2, ("if", False, 3, 4))) == 4
+
+
--- toil.py
+++ toil.py
@@ -1,2 +1,13 @@
 def evaluate(expr):
+    match expr:
+        case None | bool() | int():
+            return expr
+        case ("if", cond_expr, then_expr, else_expr):
+            return evaluate_if(cond_expr, then_expr, else_expr)
     return expr
+
+def evaluate_if(cond_expr, then_expr, else_expr):
+    if evaluate(cond_expr):
+        return evaluate(then_expr)
+    else:
+        return evaluate(else_expr)
