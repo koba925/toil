e57431d6ac8693ccde140a0dc27444e3d1c34710
Kobayashi Takahiro <koba925@gmail.com>
2026-02-11 08:24:15 +0000

    変数を実装する（スコープなし）

--- test_toil.py
+++ test_toil.py
@@ -1,16 +1,35 @@
-from toil import evaluate
+import pytest
+from toil import evaluate, new_env
+
 
 def test_evaluate_value():
-    assert evaluate(None) is None
-    assert evaluate(5) == 5
-    assert evaluate(True) is True
-    assert evaluate(False) is False
+    env = new_env()
+    assert evaluate(None, env) is None
+    assert evaluate(5, env) == 5
+    assert evaluate(True, env) is True
+    assert evaluate(False, env) is False
 
 def test_evaluate_if():
-    assert evaluate(("if", True, 2, 3)) == 2
-    assert evaluate(("if", False, 2, 3)) == 3
-    assert evaluate(("if", ("if", True, True, False), 2, 3)) == 2
-    assert evaluate(("if", True, ("if", True, 2, 3), 4)) == 2
-    assert evaluate(("if", False, 2, ("if", False, 3, 4))) == 4
+    env = new_env()
+    assert evaluate(("if", True, 2, 3), env) == 2
+    assert evaluate(("if", False, 2, 3), env) == 3
+    assert evaluate(("if", ("if", True, True, False), 2, 3), env) == 2
+    assert evaluate(("if", True, ("if", True, 2, 3), 4), env) == 2
+    assert evaluate(("if", False, 2, ("if", False, 3, 4)), env) == 4
+
+def test_evaluate_variable():
+    env = new_env()
+    assert evaluate(("define", "a", 2), env) == 2
+    assert evaluate("a", env) == 2
+
+    assert evaluate(("define", "b", True), env) == True
+    assert evaluate(("if", "b", 2, 3), env) == 2
+
+    assert evaluate(("define", "c", ("if", False, 2, 3)), env) == 3
+    assert evaluate("c", env) == 3
 
 
+def test_evaluate_undefined_variable():
+    env = new_env()
+    with pytest.raises(AssertionError):
+        evaluate("a", env)
--- toil.py
+++ toil.py
@@ -1,13 +1,45 @@
-def evaluate(expr):
+def new_env():
+    return {}
+
+def define(env, name, val):
+    env[name] = val
+    return val
+
+def lookup(env, name):
+    assert name in env, f"Undefined variable @ lookup(): {name}"
+    return env[name]
+
+
+def evaluate(expr, env):
     match expr:
         case None | bool() | int():
             return expr
+        case str(name):
+            return lookup(env, name)
+        case ("define", name, val):
+            return define(env, name, evaluate(val, env))
         case ("if", cond_expr, then_expr, else_expr):
-            return evaluate_if(cond_expr, then_expr, else_expr)
-    return expr
+            return evaluate_if(cond_expr, then_expr, else_expr, env)
+        case unexpected:
+            assert False, f"Unexpected expression @ evaluate(): {unexpected}"
 
-def evaluate_if(cond_expr, then_expr, else_expr):
-    if evaluate(cond_expr):
-        return evaluate(then_expr)
+def evaluate_if(cond_expr, then_expr, else_expr, env):
+    if evaluate(cond_expr, env):
+        return evaluate(then_expr, env)
     else:
-        return evaluate(else_expr)
+        return evaluate(else_expr, env)
+
+if __name__ == "__main__":
+    env = new_env()
+
+    print(evaluate(("define", "a", 2), env))        # 2
+    print(evaluate("a", env))                       # 2
+
+    print(evaluate(("define", "b", True), env))     # True
+    print(evaluate(("if", "b", 2, 3), env))         # 2
+
+
+    print(evaluate(("define", "c", ("if", False, 2, 3)), env))  # 3
+    print(evaluate("c", env))                       # 3
+
+
